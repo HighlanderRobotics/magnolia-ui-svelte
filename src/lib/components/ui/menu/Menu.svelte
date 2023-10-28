6c66f9f6707b0eda7edc8bacee0d19210e03369b
MangoSwirl <collin.camerono@gmail.com>
2023-10-28 14:17:08 -0700

    feat: add menus

---
+++ src/lib/components/ui/menu/Menu.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    import { cubicOut } from 'svelte/easing';
+    import { fade } from 'svelte/transition';
+
+    export let open = false;
+    export let anchor: 'left' | 'right' = 'left';
+
+    function animation(node: HTMLElement, options: { duration: number; anchor?: 'left' | 'right' }) {
+		return {
+			duration: options.duration,
+			easing: cubicOut,
+			css: (t: number) => `transform: scaleX(${(t/30)+(1-(1/30))}) scaleY(${t}); transform-origin: top ${options.anchor || 'left'};`
+		}
+	}
+</script>
+
+<svelte:window on:keydown={(e) => {
+    if (open && e.key === 'Escape') {
+        open = false;
+    }
+}} />
+
+<!-- Show the content -->
+{#if open}
+    <!-- Close when the outside is clicked -->
+    <div
+        class="overlay"
+        aria-hidden="true"
+        in:fade={{ duration: 200, delay: 200 }}
+        out:fade={{ duration: 200 }}
+        on:click={() => (open = false)}
+    />
+
+    <div
+        class="menu {anchor}"
+        in:animation={{ duration: 200, anchor }}
+        out:animation={{ duration: 50, anchor }}
+        aria-modal="true"
+    >
+        <slot />
+    </div>
+{/if}
+
+<style>
+    .menu {
+        width: 222px;
+        max-height: 140px;
+        
+        border-radius: 7px;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        background-color: var(--secondary-container);
+
+        position: absolute;
+        top: 100%;
+    }
+
+    .menu.left {
+        left: 0;
+    }
+
+    .menu.right {
+        right: 0;
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+</style>
---
+++ src/lib/components/ui/menu/MenuButton.svelte
@@ -0,0 +1,129 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	/**
+	 * The visual appearance of the button.
+	 */
+	export let variant: 'primary' | 'secondary' | 'danger' | 'text-only' | 'text-only-secondary' =
+		'secondary';
+
+	/**
+	 * The DOM element to render the button with.
+	 */
+	export let element: 'a' | 'button' = 'button';
+
+	/**
+	 * `href` attribute of an `<a>` tag. Used only for `element="a"`.
+	 */
+	export let href: string | undefined = undefined;
+
+	/**
+	 * `type` attribute of a `<button>` tag. Used only for `element="button"`.
+	 */
+	export let type: 'button' | 'submit' | 'reset' | null | undefined = undefined;
+
+	/**
+	 * Prevents the component from being interacted with.
+	 */
+	export let disabled = false;
+
+	const dispatch = createEventDispatcher();
+
+	function handleClick(event: MouseEvent) {
+		dispatch('click', event.detail);
+	}
+</script>
+
+{#if element === 'a'}
+	<a
+		class="menu-item {variant}"
+		href={disabled ? undefined : href}
+		on:click={handleClick}
+		aria-disabled={disabled}
+	>
+		<slot />
+	</a>
+{:else if element === 'button'}
+	<button
+		class="menu-item {variant}"
+		on:click={handleClick}
+		{type}
+		{disabled}
+		aria-disabled={disabled}
+	>
+		<slot />
+	</button>
+{/if}
+
+<style>
+	.menu-item {
+		width: 100%;
+		padding: 8px 14px;
+		color: var(--on-background);
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+
+		appearance: none;
+		background: none;
+		border: none;
+		cursor: pointer;
+		outline: none;
+
+		text-align: left;
+
+		transition: background-color 150ms;
+	}
+
+	.primary {
+		background-color: var(--victory-purple);
+		color: var(--background);
+	}
+
+	.primary:hover {
+		background-color: var(--victory-purple-hover);
+	}
+
+	.button[aria-disabled=true] {
+		cursor: not-allowed;
+	}
+
+	.primary[aria-disabled=true] {
+		background-color: var(--victory-purple-faded);
+		color: var(--on-victory-purple-faded);
+	}
+
+	.secondary {
+		background-color: var(--secondary-container);
+		color: var(--on-background);
+	}
+
+	.secondary:hover {
+		background-color: var(--light-gray);
+	}
+
+	.secondary[aria-disabled=true] {
+		background-color: var(--secondary-container-faded);
+		color: var(--on-light-gray-faded);
+	}
+
+	.danger {
+		background-color: var(--danger);
+		color: #ffffff;
+	}
+
+	.danger:hover {
+		background-color: var(--danger-hover);
+	}
+
+	.danger[aria-disabled=true] {
+		background-color: var(--danger-faded);
+		color: var(--on-danger-faded);
+	}
+
+	:global(.density-compact) .menu-item {
+		padding: 6px 10px;
+		font-size: 14px;
+	}
+</style>
