5e1f986c6ce7e19d7a37426af7ee675bdf65ed55
MangoSwirl <collin.camerono@gmail.com>
2023-10-28 15:06:32 -0700

    fix: export menu components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,5 +6,20 @@ import Button from './components/ui/Button.svelte';
 import Icon from './components/ui/Icon.svelte';
 import Switch from './components/ui/Switch.svelte';
 import TextField from './components/ui/TextField.svelte';
+import Menu from './components/ui/menu/Menu.svelte';
+import MenuWrapper from './components/ui/menu/MenuWrapper.svelte';
+import MenuItem from './components/ui/menu/MenuItem.svelte';
+import MenuButton from './components/ui/menu/MenuButton.svelte';
 
-export { MagnoliaUIRoot, DensityProvider, Button, Icon, Switch, TextField };
+export {
+	MagnoliaUIRoot,
+	DensityProvider,
+	Button,
+	Icon,
+	Switch,
+	TextField,
+	Menu,
+	MenuWrapper,
+	MenuItem,
+	MenuButton
+};
