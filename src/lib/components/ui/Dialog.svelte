5efd3a7bc3e7185973ab6435f8a0fd2f8641ace6
MangoSwirl <68089814+MangoSwirl@users.noreply.github.com>
2023-11-03 21:39:47 +0000

    feat: add `Dialog` component

---
+++ src/lib/components/ui/Dialog.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import { fade, fly } from "svelte/transition";
+    import { cubicOut } from "svelte/easing";
+
+    export let open = false;
+    export let canClose = true;
+</script>
+
+{#if open}
+    <div
+        class="backdrop"
+        aria-hidden="true"
+        in:fade={{ duration: 300 }}
+        out:fade={{ duration: 100 }}
+        on:click={() => open = !canClose}
+    >
+        <div class="dialog" in:fly={{ duration: 300, y: 100, easing: cubicOut }}>
+            <slot />
+        </div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+
+        background-color: #00000080;
+
+        padding: 16px;
+        display: grid;
+        place-items: center;
+    }
+
+    .dialog {
+        background-color: var(--background);
+        border-radius: 14px;
+        margin: 0 auto;
+        padding: 14px;
+        width: min(600px, calc(100vw - 32px));
+        max-height: calc(100vh - 32px);
+        overflow-y: scroll;
+    }
+</style>
